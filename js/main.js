befbe5413cbcf1002add2377fc6c5b5be529d487
EmilyTheApple <pat18010@byui.edu>
2021-04-19 16:38:21 -0600

    1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+function getList() {
 var links, text;
 links = ["/EmilyTheApple.github.io/Week1/index.html", "/EmilyTheApple.github.io/Week2/index.html", "/EmilyTheApple.github.io/Week3/index.html", "/EmilyTheApple.github.io/Week4/index.html", "/EmilyTheApple.github.io/Week5/index.html", "/EmilyTheApple.github.io/Week6/index.html"];
 
@@ -6,5 +7,9 @@ links.forEach(makeList);
 text += "</li>";
 
 function makeList(link) {
-    text += "<a>" + link + "</link>";
+    text += "<a href='" + link + "'>Weekly Code</a>";
+}
+
+document.getElementById("classLinks").innerhtml = text;
+
 }
