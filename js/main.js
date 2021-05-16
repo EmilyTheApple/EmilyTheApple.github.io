983b865cfa9739596dd3b61efb26b5f4ceaff539
EmilyTheApple <pat18010@byui.edu>
2021-05-15 22:39:18 -0600

    sdf

--- js/main.js
+++ js/main.js
@@ -73,3 +73,79 @@ const links = [
     const answer = textbox1 / textbox2;
     output.innerHTML = 'Total: ' + answer;
   }
+
+// Week 4 Team Activity
+let player = 1;
+        const boxes = document.querySelectorAll('.box');
+        const resetTicTacToe = document.getElementById('tictacreset');
+
+        // This function handles the click of the player in the tictac box.        
+        function handleClick(event) {
+            if (player == 1) {
+                event.srcElement.textContent = "X";
+                player = 2;
+            } else {
+                event.srcElement.textContent = "O";
+                player = 1;
+            }
+            this.removeEventListener('touchend', handleClick);
+            checkWinner();
+        }
+
+        function handleReset() {
+            player = 1;
+            boxEventHandler();
+        }
+
+        function boxEventHandler() {
+            boxes.forEach((box) => {
+                box.addEventListener('touchend', handleClick, false);
+                box.textContent = '';
+            });
+        }
+
+        function gameOver() {
+            alert("It was a tie... Try again...");
+        }
+
+        function checkWinner() {
+            // The winning positions
+            const winningPlay = [
+                [0, 1, 2],
+                [0, 3, 6],
+                [0, 4, 8],
+                [1, 4, 7],
+                [2, 4, 6],
+                [2, 5, 8],
+                [3, 4, 5],
+                [6, 7, 8],
+            ];
+            // Test for some filled boxes
+            let filledBox = false;
+            // Test for some empty boxes
+            let emptyBox = false;
+            // Test for the full frame with no empty spaces
+            boxes.forEach((box) => box.textContent != '' ? filledBox = true : emptyBox = true);
+
+            // Test for the winning conditions
+            winningPlay.forEach(
+                (element) => {
+                    if (boxes[element[0]].innerHTML != '' &&
+                        boxes[element[0]].innerHTML == boxes[element[1]].innerHTML &&
+                        boxes[element[0]].innerHTML == boxes[element[2]].innerHTML) {
+                        alert(`Player ${player == 1 ? 2:1} wins this round`);
+                        //Reset the game if winner is found
+                        handleReset();
+                    }
+                });
+
+            // If all boxes are full then there will be no empty boxes, alert to a tie.
+            filledBox == true && emptyBox == false ? gameOver() : null;
+        }
+
+        // The following will ONLY work on mobile devices.
+        // 'click' should be used instead of touchend but not what was in the instructions.
+        boxEventHandler();
+        //Reset the game with button click. Again, it will ONLY work on mobile
+        // The instructions were to use touchend and click not working in browser.
+        resetTicTacToe.addEventListener('touchend', handleReset, false);
