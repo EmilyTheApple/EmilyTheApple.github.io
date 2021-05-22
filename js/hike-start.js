be83756a5bc3648d5cef8d42b48d8ce8a377d925
EmilyTheApple <pat18010@byui.edu>
2021-05-21 22:16:35 -0600

    dfg

--- js/hike-start.js
+++ js/hike-start.js
@@ -1,65 +1,67 @@
-import Hike from './modules/hikes';
-const bechler = new Hike(
-    'Bechler Falls',
-    'falls.jpg',
-    'Image of Bechler Falls',
-    '3 miles',
-    'Easy',
-    'Beautiful short hike along the Bechler river to Bechler Falls',
-    'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead.',
-)
-const denanda = new Hike(
-    'Denanda Falls',
-    'falls.jpg',
-    'Image of Denanda Falls',
-    '7 miles',
-    'Moderate',
-    'Beautiful hike through Bechler Meadows river to Denanda Falls',
-    'Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead.',
-)
-const teton = new Hike(
-    'Teton Canyon',
-    'falls.jpg',
-    'Image of Bechler Falls',
-    '3 miles',
-    'Easy',
-    'Beautiful short (or long) hike through Teton Canyon.',
-    'Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead.'
-)
-const imgBasePath = "//byui-cit.github.io/cit261/examples/";
-const hikeList = [bechler, denanda, teton];
-
-function showHikeList() {
-    const hikeListElement = document.getElementById("hikes");
-    hikeListElement.innerHTML = "";
-    renderHikeList(hikeList, hikeListElement);
+class Hike {
+    constructor(name, imgSrc, imgAlt, distance, difficulty, description, directions) {
+        this.name = name;
+        this.imgSrc = imgSrc;
+        this.imgAlt = imgAlt;
+        this.distance = distance;
+        this.difficulty = difficulty;
+        this.description = description;
+        this.directions = directions;
     }
-
-function renderHikeList(hikes, parent) {
-    hikes.forEach(hike => {
-        parent.appendChild(renderOneHike(hike))
-    })
-}
-
-function renderOneHike(hike) {
-    const item = document.createElement("li");
-    
-    item.innerHTML = ` <h2>${hike.name}</h2>
-            <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+    // The HTML needed to render this object on screen
+    html() {
+        console.log("Rendering");
+        const html = ` <h2>${this.name}</h2>
+            <div class="image"><img src="${this.imgSrc}" alt="${this.imgAlt}"></div>
             <div>
                     <div>
                         <h3>Distance</h3>
-                        <p>${hike.distance}</p>
+                        <p>${this.distance}</p>
                     </div>
                     <div>
                         <h3>Difficulty</h3>
-                        <p>${hike.difficulty}</p>
+                        <p>${this.difficulty}</p>
                     </div>
             </div>`;
-    
-    return item;
+        return html;
+    }
 }
-
-window.addEventListener("load", () => {
-    hikeList.showHikeList();
-});
+// Create a bulleted list that displays the hike information
+Array.prototype.show = function () {
+    const hikeListElement = document.getElementById("hikes");
+    hikeListElement.innerHTML = "";
+    this.forEach(hike => {
+        const item = document.createElement("li");
+        item.innerHTML = hike.html();
+        hikeListElement.appendChild(item);
+    });
+}
+const hikeList = [
+    new Hike(
+        "Bechler Falls",
+        "https://www.americansouthwest.net/wyoming/photographs700/cave-falls2.jpg",
+        "Image of Bechler Falls",
+        "3 miles",
+        "Easy",
+        "Beautiful short hike along the Bechler river to Bechler Falls",
+        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."),
+    new Hike(
+        "Teton Canyon",
+        "https://www.americansouthwest.net/wyoming/photographs700/cave-falls2.jpg",
+        "Image of Bechler Falls",
+        "3 miles",
+        "Easy",
+        "Beautiful short (or long) hike through Teton Canyon.",
+        "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Staline Raod for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
+    ),
+    new Hike(
+        "Denanda Falls",
+        "https://www.americansouthwest.net/wyoming/photographs700/cave-falls2.jpg",
+        "Image of Bechler Falls",
+        "7 miles",
+        "Moderate",
+        "Beautiful hike through Bechler meadows river to Denanda Falls",
+        "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+    )
+];
+hikeList.show();
